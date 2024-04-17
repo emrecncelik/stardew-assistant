e48e253ac43563187e07633906f3bc6f0fe41883
emrecncelik <emrecncelik@gmail.com>
2024-04-17 14:10:34 +0300

    add html download for article pages, add save load funcs

--- download_wiki_pages.py
+++ download_wiki_pages.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 
 # TODO: Download html content of all pages extracted
 
+GET_ARTICLE_NAMES = False
+DOWNLOAD_ARTICLE_PAGES = True
 BASE_URL = "https://stardewvalleywiki.com"
 
 
@@ -42,27 +44,59 @@ def read_json_as_dict(filename: str) -> dict:
     return data
 
 
+def save_html(filename: str, html_content: str) -> None:
+    with open(filename, "w") as f:
+        f.write(html_content)
+
+
+def read_html(filename: str) -> BeautifulSoup:
+    with open(filename, "r") as f:
+        return BeautifulSoup(f.read(), "html.parser")
+
+
 if __name__ == "__main__":
-    i = 0
-    main_page = "https://stardewvalleywiki.com/mediawiki/index.php?title=Special:AllPages&hideredirects=1"
-    article_pages = {}
 
-    while True:
-        # TODO: Add a logic to end this when it reaches the end of the pages
-        # TODO: Maybe respect a little and add sleep time?
-        # TODO: This gets 1990 pages, wiki says 1992. Why?
-        main_page_html = get_page(main_page)
-        article_pages_temp = find_pages(main_page_html)
-        old_main_page = main_page
-        main_page = BASE_URL + find_next_page(main_page_html)
+    if GET_ARTICLE_NAMES:
+        i = 0
+        main_page = "https://stardewvalleywiki.com/mediawiki/index.php?title=Special:AllPages&hideredirects=1"
+        article_pages = {}
 
-        article_pages[old_main_page] = article_pages_temp
-        save_dict_as_json("article_pages.json", article_pages)
+        while True:
+            # TODO: Add a logic to end this when it reaches the end of the pages
+            # TODO: Maybe respect a little and add sleep time?
+            # TODO: This gets 1990 pages, wiki says 1992. Why?
+            main_page_html = get_page(main_page)
+            article_pages_temp = find_pages(main_page_html)
+            old_main_page = main_page
+            main_page = BASE_URL + find_next_page(main_page_html)
 
-        print(f"#" * 50)
-        print(f"Page {i} done!")
-        print(f"First 10: " + str(article_pages_temp[:10]))
-        print(f"Last 10: " + str(article_pages_temp[-10:]))
-        print(f"#" * 50)
+            article_pages[old_main_page] = article_pages_temp
+            save_dict_as_json("data/article_pages.json", article_pages)
 
-        i += 1
+            print(f"#" * 50)
+            print(f"Page {i} done!")
+            print(f"First 10: " + str(article_pages_temp[:10]))
+            print(f"Last 10: " + str(article_pages_temp[-10:]))
+            print(f"#" * 50)
+
+            i += 1
+
+    if DOWNLOAD_ARTICLE_PAGES:
+        article_names = read_json_as_dict("data/article_pages.json")
+        article_pages = []
+
+        for a in article_names.values():
+            article_pages.extend(a)
+
+        del article_names
+
+        for i, page in enumerate(article_pages):
+            if i < 269:
+                continue
+            print(f"#" * 50)
+            print(f"Downloading page {i} of {len(article_pages)}: {page}")
+            html_content = get_page(BASE_URL + page)
+            page = page.replace("/", "")
+            save_html(f"data/article_pages/{page}.html", html_content)
+            print(f"Page {i} done!")
+            print(f"#" * 50)
